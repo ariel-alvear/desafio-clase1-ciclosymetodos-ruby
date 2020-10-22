5c6666787c89bba54e10127ec5df1c0e949a8124
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-22 08:59:05 -0300

    ahora sí, finalizado el desafio. Había entendido mal una instrucción, pero ahora lo corregí

--- fuerza_bruta.rb
+++ fuerza_bruta.rb
@@ -1,54 +1,10 @@
-password = ARGV[0].length
-number1 = password * 26
+password = ARGV[0]
 letter = "a"
+sum = 0
 
-suma = 0
-
-if password == 1
-  while (letter != "aa")
-    letter = letter.next
-    suma +=1
-  end
-elsif password == 2
-  while (letter != "aaa")
-    letter = letter.next
-    suma += 1
-  end
-elsif password == 3
-  while (letter != "aaaa")
-    letter = letter.next
-    suma += 1
-  end
-elsif password == 4
-  while (letter != "aaaaa")
-    letter = letter.next
-    suma += 1
-  end
-elsif password == 5
-  while (letter != "aaaaaa")
-    letter = letter.next
-    suma += 1
-  end
-elsif password == 6
-  while (letter != "aaaaaaa")
-    letter = letter.next
-    suma += 1
-  end
-elsif password == 7
-  while (letter != "aaaaaaaa")
-    letter = letter.next
-    suma += 1
-  end
-elsif password == 8
-  while (letter != "aaaaaaaaa")
-    letter = letter.next
-    suma += 1
-  end
-elsif password == 9
-  while (letter != "aaaaaaaaaa")
-    letter = letter.next
-    suma += 1
-  end
+while letter != password
+  letter = letter.next
+  sum += 1
 end
 
-puts "los intentos necesarios para hackear una clave de #{password} dígitos, es #{suma}"
+puts sum
