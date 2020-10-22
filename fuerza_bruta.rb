0fbe2aff882d8e1a0f4882903275e5d358b96528
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-21 22:37:38 -0300

    seguimos luchando con el desafio

--- fuerza_bruta.rb
+++ fuerza_bruta.rb
@@ -1,7 +1,27 @@
-password = ARGV[0].length * 26
+password = ARGV[0].length
+acum = 1
 
-i = 0
-
-while i < password
-
+#n! / a!b!c!
+productonumerador = 1
+for i in (1..26)
+  print i
+  puts productonumerador
+  productonumerador *= acum + 1
 end
+
+productodenominador1 = 1
+for f in (1..password)
+  productodenominador1 *= f
+end
+
+productodenominador2 = 1
+for g in (1..27)
+  productodenominador2 *= g
+end
+
+
+puts productonumerador
+puts productodenominador1
+puts productodenominador2
+puts "\n"
+puts productonumerador / (productodenominador1 * productodenominador2)
